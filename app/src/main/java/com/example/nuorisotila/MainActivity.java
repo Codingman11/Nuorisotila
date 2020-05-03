c344c4e3475ef5592ee2d6da9b71b348b3cd2b19
JessePel <jesse1.peltola@outlook.com>
2020-05-03 16:48:25 +0300

    Making Event UI part 2

--- app/src/main/java/com/example/nuorisotila/MainActivity.java
+++ app/src/main/java/com/example/nuorisotila/MainActivity.java
@@ -9,6 +9,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -58,6 +61,17 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        //Add event
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setAlpha(0.7f);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
 
